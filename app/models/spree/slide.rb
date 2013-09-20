cc33e02a73a5258bae1cd610dde418c4aa5682d7
Dustin Platte <dustinplatte@gmail.com>
2013-09-19 18:41:29 -0700

    Fixing strong parameters

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -1,7 +1,7 @@
 class Spree::Slide < ActiveRecord::Base
   has_attached_file :image
   scope :published, where(:published => true)
-  params :name, :body, :link_url, :published, :image, :position, :product_id
+  params.permit(:name, :body, :link_url, :published, :image, :position, :product_id)
   belongs_to :product
 
   def initialize(attrs = nil)
